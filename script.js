12cc7609601fa102231183b5aaa9203d42c05dee
Pirjo Mure <N5589@student.jamk.fi>
2023-01-26 13:43:45 +0200

    updated

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
 let js='amazing';
-if(js ==='amazing') alert('Javascript is FUN!');
-
-40+8+23-10;
 console.log(40+8+23-10);
+
+let firstName="Pipe";
+console.log(firstName);
+
+//Lecture, Values and Variables
+var country="Finland";
+var continent="Europe";
+var population=5500000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//Lecture, Data Types
+var isIsland=false;
+let language;
+console.log(isIsland);
+console.log(population);
+console.log(country)
+console.log(language);
+
+//Lecture, let, const and var
+language="Finnish";
+console.log(language);
+
+//Lecture, Basic Operators
+population=population/2;
+console.log(population);
+console.log(typeof country);
+
+const now=2023;
+const agePipe=now-1980;
+const ageSarah=now-2018;
+console.log(ageSarah, agePipe);
+console.log(ageSarah *2, ageSarah / 2, 2**3);
+//2**3 is 3=2*2*2
+
+
+
+
