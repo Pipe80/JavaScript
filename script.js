4d2263055014ed740aff0f1ba8e1218ae0b8e095
Pirjo Mure <N5589@student.jamk.fi>
2023-01-31 09:22:14 +0200

    update

--- script.js
+++ script.js
@@ -149,3 +149,26 @@ if(ajaako)
 console.log('Sarah saapi ajjaa');
 else
 console.log('joku toinen rattihin');
+
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+const dolphins =(96 + 108 + 89) / 3;
+console.log(dolphins);
+
+const koalas=(88 + 91 + 110) / 3;
+console.log(koalas);
+
+if (dolphins > koalas) {console.log('Dolphins wins!');}
+if (koalas === dolphins) {console.log('Its a tie');}
+if (koalas > dolphins) {console.log('Koalas wins!');}  
--- script2.js
+++ script2.js
@@ -1,22 +1,7 @@
-// Coding Challenge #3
-// There are two gymnastics teams, Dolphins and Koalas. They compete against each
-// other 3 times. The winner with the highest average score wins a trophy!
-// Your tasks:
-// 1. Calculate the average score for each team, using the test data below
-// 2. Compare the team's average scores to determine the winner of the competition,
-// and print it to the console. Don't forget that there can be a draw, so test for that
-// as well (draw means they have the same average score)
-// Test data:
-// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
-// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
-// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+const age=23;
+// age>=18?console.log('adult😀'):
+// console.log('teenager😂');
 
-const dolphins =(96 + 108 + 89) / 3;
-console.log(dolphins);
+const drink= age >=18 ? 'coffee':'water';
+console.log(drink);
 
-const koalas=(88 + 91 + 110) / 3;
-console.log(koalas);
-
-if (dolphins > koalas) {console.log('Dolphins wins!');}
-if (koalas === dolphins) {console.log('Its a tie');}
-if (koalas > dolphins) {console.log('Koalas wins!');}  
